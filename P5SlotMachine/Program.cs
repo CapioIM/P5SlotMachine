830fad9aa4079b69cf08d5f7357cbc32f72a421e
CapioIM <igorsmalinovskis9@gmail.com>
2023-07-29 17:56:21 +0100

    Add project files.

---
+++ P5SlotMachine/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace P5SlotMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and Welcome to Slot Machine Game!");
+        }
+
+    }
+}
