e03df6d6cbffa3ea1a272bd048eb3afae934528a
CapioIM <igorsmalinovskis9@gmail.com>
2023-08-02 21:53:14 +0100

    fix column (vertical) loop

--- P5SlotMachine/Program.cs
+++ P5SlotMachine/Program.cs
@@ -3,12 +3,12 @@
     internal class Program
     {
         static Random rand = new Random();
-        const int VERTICAL_LINES_IN_GAME = 3;      // how many vertical lines
-        const int HORIZONTAL_LINES_IN_GAME = 3;     //how many horizontal lines
+        const int COLUMNS_LINES_IN_GAME = 3;      // how many vertical lines
+        const int ROWS_LINES_IN_GAME = 5;     //how many horizontal lines
         static void Main(string[] args)
         {
             string[] slotSymbols = { "0" };//, "1", "2", "3", "4", "5", "6", "7", "8", "9", "J" };             // test symbols
-            string[,] slotMachineArray = new string[VERTICAL_LINES_IN_GAME, HORIZONTAL_LINES_IN_GAME];          //slot machine array size
+            string[,] slotMachineArray = new string[COLUMNS_LINES_IN_GAME, ROWS_LINES_IN_GAME];          //slot machine array size
 
             while (true)
             {
@@ -18,12 +18,12 @@
                 Console.WriteLine();
 
                 int amountOfLanesPlay = Convert.ToInt32(Console.ReadLine());
-                int rightMinusOne = HORIZONTAL_LINES_IN_GAME - 1;         //loop doesnt run out of array range
-                int downMinusOne = VERTICAL_LINES_IN_GAME - 1;
+                int rightMinusOne = ROWS_LINES_IN_GAME - 1;         //loop doesnt run out of array range
+                int downMinusOne = COLUMNS_LINES_IN_GAME - 1;
 
-                for (int down = 0; down < VERTICAL_LINES_IN_GAME; down++)                //fill array with random strings from slotSymbols array
+                for (int down = 0; down < COLUMNS_LINES_IN_GAME; down++)                //fill array with random strings from slotSymbols array
                 {
-                    for (int right = 0; right < HORIZONTAL_LINES_IN_GAME; right++)
+                    for (int right = 0; right < ROWS_LINES_IN_GAME; right++)
                     {
                         int randomIndex = rand.Next(slotSymbols.Length);
                         slotMachineArray[down, right] = slotSymbols[randomIndex];
@@ -32,17 +32,16 @@
                     Console.WriteLine();
                 }
 
-                int rightLineMatching = 0;               //horizontal -  line check and win output
-
-                int rowsToPlay = VERTICAL_LINES_IN_GAME;
-                if (amountOfLanesPlay < HORIZONTAL_LINES_IN_GAME)
+                int rowsToPlay = COLUMNS_LINES_IN_GAME;            //run for loop under for amount of times selected
+                if (amountOfLanesPlay < COLUMNS_LINES_IN_GAME)
                 {
                     rowsToPlay = amountOfLanesPlay;
                 }
 
-                for (int down = 0; down < rowsToPlay; down++)                  //horizontal lines check/win output
+                int rightLineMatching = 0;                      //horizontal -  line check and win output
+                for (int down = 0; down < rowsToPlay; down++)
                 {
-                    for (int right = 0; right < HORIZONTAL_LINES_IN_GAME; right++)
+                    for (int right = 0; right < ROWS_LINES_IN_GAME; right++)
                     {
                         if (right != rightMinusOne && slotMachineArray[down, right] == slotMachineArray[down, right + 1])
                         {
@@ -59,16 +58,17 @@
                         }
                     }
                 }
-                int downLineMatching = 0;              //vertical | line check and win output    
-                int columnsToPlay = HORIZONTAL_LINES_IN_GAME;
-                if (amountOfLanesPlay - HORIZONTAL_LINES_IN_GAME < VERTICAL_LINES_IN_GAME)
+
+                int columnsToPlay = ROWS_LINES_IN_GAME;   //run loop under when amountOfLanesPlay > 3 for amount of times = to width of array
+                if (amountOfLanesPlay - COLUMNS_LINES_IN_GAME < ROWS_LINES_IN_GAME)
                 {
-                    columnsToPlay = amountOfLanesPlay - HORIZONTAL_LINES_IN_GAME;
+                    columnsToPlay = amountOfLanesPlay - COLUMNS_LINES_IN_GAME;
                 }
 
+                int downLineMatching = 0;              //vertical | line check and win output    
                 for (int right = 0; right < columnsToPlay; right++)
                 {
-                    for (int down = 0; down < VERTICAL_LINES_IN_GAME; down++)
+                    for (int down = 0; down < COLUMNS_LINES_IN_GAME; down++)
                     {
                         if (down != downMinusOne && slotMachineArray[down, right] == slotMachineArray[down + 1, right])
                         {
