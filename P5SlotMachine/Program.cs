a69a4dbba7e226177d7cb9785682133e6a9b6634
CapioIM <igorsmalinovskis9@gmail.com>
2023-09-12 12:44:19 +0100

    changed row line matching method

--- P5SlotMachine/Program.cs
+++ P5SlotMachine/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Transactions;
 
 namespace P5SlotMachine
 {
@@ -11,12 +12,12 @@ namespace P5SlotMachine
         const int PLAYER_STARTING_BALANCE = 50;
         static void Main(string[] args)
         {
-            string[] slotSymbols = { "0" };//, "1", "2", "3", "4", "5", "6", "7", "8", "9", "J" };             // test symbols
-            string[,] slotMachineArray = new string[ROW_LINES_IN_GAME, COLUMN_LINES_IN_GAME];          //slot machine array size
+            int[] slotSymbols = { 0 };//, 1, 2, 3, 4, 5, 6, 7, 8, 9};             // test symbols
+            int[,] slotMachineArray = new int[ROW_LINES_IN_GAME, COLUMN_LINES_IN_GAME];          //slot machine array size
             int rowAndColumnLines = COLUMN_LINES_IN_GAME + ROW_LINES_IN_GAME;                         //rows and column lines amount
-            int playerBalance = PLAYER_STARTING_BALANCE;
-            double betPerLane = 0;
+            double playerBalance = PLAYER_STARTING_BALANCE;
             int amountOfLanesPlay = 0;
+            double betPerLane = 0;
 
             while (true)
             {
@@ -27,24 +28,24 @@ namespace P5SlotMachine
                     $" Lines {rowAndColumnLines + 1}-{rowAndColumnLines + DIAGONAL_LINES_IN_GAME} vertical, horizontal and diagonal");
                 Console.WriteLine();
                 Console.WriteLine($"Your Balance is {playerBalance} !");
-
-                while (true)  // loop for bet as long as there is enough of balance
+                Console.WriteLine();
+                while (true)  // loop for bet as long as there is enough of balance program continues
                 {
                     Console.WriteLine("How many lines would you like to play?");
                     Console.WriteLine();
-                    amountOfLanesPlay = Convert.ToInt32(Console.ReadLine());
+                    amountOfLanesPlay = Convert.ToInt32(Console.ReadLine());                                    //enter how many lanes to play
                     if (amountOfLanesPlay > ROW_LINES_IN_GAME + COLUMN_LINES_IN_GAME + DIAGONAL_LINES_IN_GAME)  // amountOfLanesPlay cannot be more than max amount of lanes to play
                     {
                         amountOfLanesPlay = ROW_LINES_IN_GAME + COLUMN_LINES_IN_GAME + DIAGONAL_LINES_IN_GAME;
                     }
 
                     Console.WriteLine("How much would you like to bet per lane?");
-                    betPerLane = Convert.ToInt32(Console.ReadLine());
-                    if (playerBalance < amountOfLanesPlay * betPerLane)
+                    betPerLane = Convert.ToInt32(Console.ReadLine());                                           //enter bet per lane
+                    if (playerBalance < amountOfLanesPlay * betPerLane)                                         // balance has to be greater than bet * lanes
                     {
                         Console.WriteLine("You have insufficient funds! Please place lower bet!");
                     }
-                    else
+                    else                                                                                        // continue program if bet amount is less than money balance 
                     {
                         break;
                     }
@@ -52,10 +53,9 @@ namespace P5SlotMachine
                 }
                 Console.WriteLine();
 
-                Console.WriteLine();
                 int columnMinusOne = COLUMN_LINES_IN_GAME - 1;         //loop doesnt run out of array range
-                int rowMinusOne = ROW_LINES_IN_GAME - 1;
-
+                int rowMinusOne = ROW_LINES_IN_GAME - 1;                //loop doesnt run out of array range
+              //  double winAmount = 0;
 
                 for (int rowIndex = 0; rowIndex < ROW_LINES_IN_GAME; rowIndex++)                //fill array with random strings from slotSymbols array
                 {
@@ -75,6 +75,7 @@ namespace P5SlotMachine
                 }
 
                 int rightLineMatching = 0;                      //horizontal -  line check and win output
+                /*
                 for (int rowIndex = 0; rowIndex < rowsToPlay; rowIndex++)
                 {
                     for (int columnIndex = 0; columnIndex <= columnMinusOne; columnIndex++)
@@ -85,7 +86,8 @@ namespace P5SlotMachine
                             if (rightLineMatching == columnMinusOne)
                             {
 
-                                Console.WriteLine($"Win - line nr: {rowIndex + 1}");
+                                Console.WriteLine($"Win - line nr: {rowIndex + 1}"); 
+                                //if (slotMachineArray[])
                             }
                         }
                         else
@@ -95,6 +97,27 @@ namespace P5SlotMachine
                         }
                     }
                 }
+*/
+                int rowCharCheck = 0;
+                int rowLineMatch = 0;
+                for (int rowIndex = 0; rowIndex < rowsToPlay; rowIndex++)
+                {
+                    rowCharCheck = slotMachineArray[rowIndex, 0];
+                    int slotsMatched = 0;
+                    for(int columnIndex = 0; columnIndex < COLUMN_LINES_IN_GAME;columnIndex++)
+                    {
+                        if (rowCharCheck == slotMachineArray[rowIndex, columnIndex])
+                        {
+                            slotsMatched++;
+                            if (slotsMatched == columnMinusOne)
+                            {
+                                Console.WriteLine($"Win - line nr: {rowIndex + 1}");
+                            }
+                        }
+
+                    }
+                }
+
 
                 int columnsToPlay = COLUMN_LINES_IN_GAME;   //run loop under when amountOfLanesPlay > 3 for amount of times = to width of array
                 if (amountOfLanesPlay - ROW_LINES_IN_GAME < COLUMN_LINES_IN_GAME)
